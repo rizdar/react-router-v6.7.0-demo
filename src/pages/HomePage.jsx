9f353f1c0f3fdd1df1b7fb103eb93b892a5a3ebf
Rizki Darmawan <rizdar.contact@gmail.com>
2023-01-20 23:13:51 +0700

    use navlink and navigate programaticly

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  };
+
   return (
     <>
       <h2>My Home Pages</h2>
       <p>
         Go to products page <Link to="/products">Products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
